b6aaf057426f1dfabf0e39b54fa6b02dc35e5fa4
Jarno Gruppwn <jgruppen@hotmail.com>
2023-07-03 11:46:00 +0200

    Custom Environment Variables

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Custom Environment Variables</h1>
+      <div>REACT_APP_BACKEND_URI: {process.env.REACT_APP_BACKEND_URI}</div>
+      <div>REACT_APP_AAD_AUTHORITY: {process.env.REACT_APP_AAD_AUTHORITY}</div>
+      <div>REACT_APP_AAD_CLIENT_ID: {process.env.REACT_APP_AAD_CLIENT_ID}</div>   
     </div>
   );
 }
